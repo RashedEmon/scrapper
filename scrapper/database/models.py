a8062baf462dc8a9de295e3d1157d20bdb5fd4f4
rashed <rashedul@w3engineers.com>
2024-07-02 20:00:25 +0600

    # Feature - Field type changed.

--- scrapper/database/models.py
+++ scrapper/database/models.py
@@ -41,15 +41,15 @@ class GolfCourse(Base):
 class TeeTime(Base):
     __tablename__ = 'tee_times'
 
-    course_id = Column(Integer, ForeignKey('golf_courses.course_id'), nullable=False)
-    tee_time_id = Column(Integer, nullable=False)
+    course_id = Column(INTEGER, ForeignKey('golf_courses.course_id'), nullable=False)
+    tee_time_id = Column(INTEGER, nullable=False)
     tee_datetime = Column(TIMESTAMP)
     display_rate = Column(REAL, nullable=False)
     currency = Column(VARCHAR(10), nullable=False)
     display_fee_rate = Column(BOOLEAN, nullable=True)
-    max_transaction_fee = Column(Float, nullable=True)
-    hole_count = Column(Integer, nullable=True)
-    rate_name = Column(String, nullable=True)
+    max_transaction_fee = Column(REAL, nullable=True)
+    hole_count = Column(INTEGER, nullable=True)
+    rate_name = Column(VARCHAR(100), nullable=True)
     updated_at = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now(), nullable=False)
     created_at = Column(TIMESTAMP, server_default=func.now(), nullable=False)
 
